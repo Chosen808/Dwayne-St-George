21a409fc5fb6deed13cb2747e46d026be0acd9aa
Chosen808 <learnkingdom@gmail.com>
2023-10-03 13:43:28 -0400

    Commit - 31023 - 1343

--- myprojects/algorithms/general-combinatorial-algorithms/cycleDetection.c
+++ myprojects/algorithms/general-combinatorial-algorithms/cycleDetection.c
@@ -81,18 +81,37 @@ main ()
     initialize ();
 
     // Creating a linked list
-    insert (8);
-    insert (3);
-    insert (2);
-    insert (7);
-    insert (9);
+    insert (54);
+    insert (1);
+    insert (10);
+    insert (123);
+    insert (54);
 
     /*
     Create loop in linked list. Set next pointer of last node to second node from head
     */
-   head->next->next->next->next->next = head->next;
+    head->next->next->next->next->next = head;
+    
+    printf ("head->data = %d \n", head->data);
+    printf ("head->next->data = %d \n", head->next->data);
+    printf ("head->next->next->data = %d \n", head->next->next->data);
+    printf ("head->next->next->next->data = %d \n", head->next->next->next->data);
+    printf ("head->next->next->next->next->data = %d \n", head->next->next->next->next->data);
+    printf ("head->next->next->next->next->next->data = %d \n", head->next->next->next->next->next->data);
+    printf ("head->next->next->next->next->next->next->data = %d \n", head->next->next->next->next->next->next->data);
 
-   findloop (head);
+    findloop (head);
 
-   return 0;
+    printf ("head->NEXT = %p \n", head->next);
+
+    findloop (head);
+
+    printf ("Head->next->next = head->next %p %p \n", head->next->next = head->next, __builtin_frame_address(0));
+    printf ("head->next->next->next = %p %p \n", head->next->next->next, __builtin_frame_address(0));
+
+    findloop (head);
+
+    //printLinkedlist (head->next);
+
+     return 0;
 }
